e47f6e008ca263b7acd98de2b33b1ad89e83f93a
AjayPradosh <ajaypradosh7@gmail.com>
2025-02-21 16:40:55 +0530

    some changes

--- asset/js/function.js
+++ asset/js/function.js
@@ -12,3 +12,38 @@ function learnMore() {
     console.log("Learn More");
     window.location.href = "idukki.html";
 }
+
+$(document).ready(function() {
+    $('#emailbtn').click(function() {
+        console.log('clicked');
+        $('#email').removeClass('d-none');
+    });
+
+    $('#sentMail').click(function(e) {
+        e.preventDefault();
+
+        var email = $('#emailinput').val();
+        var message = $('#message').val();
+        let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (emailPattern.test(email)) {
+            
+            Swal.fire({
+                title: 'Success!',
+                text: 'Your email has been sent',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+            });
+            $('#emailinput').removeClass('border border-danger');
+        } else {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Please enter a valid email',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            });
+            $('#emailinput').addClass('border border-danger');
+            // $('#emailinput').focus();
+        }
+    });
+});
